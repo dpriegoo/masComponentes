3f8fecd58eb6c88d43ef0d771d8889f390de4b5d
dpriegoo <77279312+dpriegoo@users.noreply.github.com>
2021-10-19 14:26:57 +0200

    Imagenes y lista

---
+++ app/src/main/java/com/example/mascomponentes/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.mascomponentes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+        Spinner lstColores = findViewById(R.id.spinner);
+
+        lstColores.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+
+    }
+}
